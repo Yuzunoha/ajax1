8cc2135c039a03d0303a795256b6e6bdd8e0a710
Yuzunoha <studysteadystandby@gmail.com>
2021-03-07 16:16:12 +0900

    外部ファイルにした

--- frontend/main.js
+++ frontend/main.js
@@ -5,19 +5,4 @@ const p = console.log;
 
 p1.innerHTML = 'jsから編集しました。';
 
-const jsPost = () => {
-  const url = 'http://localhost:20280/api/json1';
-  const obj = { hello: 'world' };
-  const method = 'POST';
-  const body = JSON.stringify(obj);
-  const headers = {
-    Accept: 'application/json',
-    'Content-Type': 'application/json',
-  };
-  fetch(url, { method, headers, body })
-    .then((res) => res.json())
-    .then(console.log)
-    .catch(console.error);
-};
-
 jsPost();
