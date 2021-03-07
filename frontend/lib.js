611b8fabe2cd9fb4238a438cb1d8cd70755f7d54
Yuzunoha <studysteadystandby@gmail.com>
2021-03-07 19:50:25 +0900

    できた

--- frontend/lib.js
+++ frontend/lib.js
@@ -10,8 +10,8 @@ const jsPost = (body) => {
   const method = 'POST';
   return fetch(url, { method, headers, body })
     .then((res) => res.json())
-    .then(console.log)
-    .catch(console.error);
+    .then((json) => json)
+    .catch((err) => err);
 };
 
 const jsGet = () => {
@@ -28,5 +28,47 @@ const send = () => {
   const content = document.getElementById('content').value;
   const obj = { name, address, content };
   const json = JSON.stringify(obj);
-  jsPost(json);
+  jsPost(json).then(() => {
+    jsGet().then((json) => {
+      const objects = jsonToObjects(json);
+      generateTable(objects);
+    });
+  });
+};
+
+const generateThTd = (thortd, text) => {
+  const thtd = document.createElement(thortd);
+  const textNode = document.createTextNode(text);
+  thtd.appendChild(textNode);
+  return thtd;
+};
+
+const generateTable = (objects) => {
+  const div = document.getElementById('div1');
+  const table = document.createElement('table');
+  const tbody = document.createElement('tbody');
+  const trHeader = document.createElement('tr');
+  trHeader.appendChild(generateThTd('th', 'name'));
+  trHeader.appendChild(generateThTd('th', 'address'));
+  trHeader.appendChild(generateThTd('th', 'content'));
+  tbody.appendChild(trHeader);
+  for (const object of objects) {
+    const tr = document.createElement('tr');
+    tr.appendChild(generateThTd('td', object.name));
+    tr.appendChild(generateThTd('td', object.address));
+    tr.appendChild(generateThTd('td', object.content));
+    tbody.appendChild(tr);
+  }
+  table.appendChild(tbody);
+  table.setAttribute('border', '1');
+  div1.innerHTML = '';
+  div.appendChild(table);
+};
+
+const jsonToObjects = (json) => {
+  const a = [];
+  for (let i = 0; i < json.length; i++) {
+    a.push(JSON.parse(json[i].json));
+  }
+  return a;
 };
--- frontend/main.js
+++ frontend/main.js
@@ -1,48 +1,8 @@
 'use strict';
 
-const generateThTd = (thortd, text) => {
-  const thtd = document.createElement(thortd);
-  const textNode = document.createTextNode(text);
-  thtd.appendChild(textNode);
-  return thtd;
-};
-
-const generateTable = (div, objects) => {
-  const table = document.createElement('table');
-  const tbody = document.createElement('tbody');
-  const trHeader = document.createElement('tr');
-  trHeader.appendChild(generateThTd('th', 'name'));
-  trHeader.appendChild(generateThTd('th', 'address'));
-  trHeader.appendChild(generateThTd('th', 'content'));
-  tbody.appendChild(trHeader);
-  for (const object of objects) {
-    const tr = document.createElement('tr');
-    tr.appendChild(generateThTd('td', object.name));
-    tr.appendChild(generateThTd('td', object.address));
-    tr.appendChild(generateThTd('td', object.content));
-    tbody.appendChild(tr);
-  }
-  table.appendChild(tbody);
-  table.setAttribute('border', '1');
-  div1.innerHTML = '';
-  div.appendChild(table);
-};
-
-const jsonToObjects = (json) => {
-  const a = [];
-  for (let i = 0; i < json.length; i++) {
-    a.push(JSON.parse(json[i].json));
-  }
-  return a;
-};
-
 const main = () => {
-  const div1 = document.getElementById('div1');
-  jsPost();
   jsGet().then((json) => {
-    const objects = jsonToObjects(json);
-    generateTable(div1, objects);
-    console.log(json);
+    generateTable(jsonToObjects(json));
   });
 };
 
