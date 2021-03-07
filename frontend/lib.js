b55d5515f68042faaa322adbde0715178d9b3e1d
Yuzunoha <studysteadystandby@gmail.com>
2021-03-07 17:17:33 +0900

    整理した

--- frontend/lib.js
+++ frontend/lib.js
@@ -2,13 +2,14 @@
 
 const jsPost = () => {
   const url = 'http://localhost:20280/api/json1';
-  const obj = { hello: 'world' };
   const method = 'POST';
-  const body = JSON.stringify(obj);
   const headers = {
     Accept: 'application/json',
     'Content-Type': 'application/json',
   };
+  const body = JSON.stringify({
+    hello: 'world',
+  });
   fetch(url, { method, headers, body })
     .then((res) => res.json())
     .then(console.log)
