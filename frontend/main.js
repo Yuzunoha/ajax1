a3a52e4856ed973ea00ce8e03a0a5116a1420086
Yuzunoha <studysteadystandby@gmail.com>
2021-03-07 18:37:27 +0900

    slimにした

--- frontend/main.js
+++ frontend/main.js
@@ -1,39 +1,22 @@
 'use strict';
 
 const generateTable = (div) => {
-  // body への参照を取得します
-  var mybody = document.getElementsByTagName('body')[0];
-
-  // <table> と <tbody> 要素を生成します
-  const mytable = document.createElement('table');
-  const mytablebody = document.createElement('tbody');
-
-  // 全てのセルを生成します
-  for (var j = 0; j < 2; j++) {
-    // <tr> 要素を生成します
-    const mycurrent_row = document.createElement('tr');
-
-    for (var i = 0; i < 2; i++) {
-      // <td> 要素を生成します
-      const mycurrent_cell = document.createElement('td');
-      // テキストノードを生成します
-      const currenttext = document.createTextNode('cell is row ' + j + ', column ' + i);
-      // 生成したテキストノードを <td> セルへと付加します
-      mycurrent_cell.appendChild(currenttext);
-      // その <td> セルを <tr> 行へと付加します
-      mycurrent_row.appendChild(mycurrent_cell);
+  const table = document.createElement('table');
+  const tbody = document.createElement('tbody');
+  for (let j = 0; j < 2; j++) {
+    const tr = document.createElement('tr');
+    for (let i = 0; i < 2; i++) {
+      const td = document.createElement('td');
+      const textNode = document.createTextNode('cell is row ' + j + ', column ' + i);
+      td.appendChild(textNode);
+      tr.appendChild(td);
     }
-    // その <tr> 行を <tbody> へと付加します
-    mytablebody.appendChild(mycurrent_row);
+    tbody.appendChild(tr);
   }
-
-  // <tbody> を <table> へと付加します
-  mytable.appendChild(mytablebody);
-  // mytable の border 属性を 2 に設定します
-  mytable.setAttribute('border', '2');
-  // <table> を <body> へと付加します
+  table.appendChild(tbody);
+  table.setAttribute('border', '1');
   div1.innerHTML = '';
-  div.appendChild(mytable);
+  div.appendChild(table);
 };
 
 const main = () => {
