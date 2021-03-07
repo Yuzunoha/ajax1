1cc758c41efb9333589a7c2eedff7b52b06e7bce
Yuzunoha <studysteadystandby@gmail.com>
2021-03-07 17:13:14 +0900

    window.addEventListener('load', main, false);

--- frontend/main.js
+++ frontend/main.js
@@ -1,8 +1,9 @@
 'use strict';
 
-const p1 = document.getElementById('p1');
-const p = console.log;
+const main = () => {
+  const p1 = document.getElementById('p1');
+  p1.innerHTML = 'jsから編集しました。';
+  jsPost();
+};
 
-p1.innerHTML = 'jsから編集しました。';
-
-jsPost();
+window.addEventListener('load', main, false);
