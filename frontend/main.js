3a1e089f0c041307e4c73644e95e22ecd7f70069
Yuzunoha <studysteadystandby@gmail.com>
2021-03-08 13:50:44 +0900

    res.json()はPromise

--- frontend/main.js
+++ frontend/main.js
@@ -1,41 +1,23 @@
 'use strict';
 
-const url = 'https://www.jma.go.jp/bosai/forecast/data/overview_week/130000.json';
-
-const test1 = () => {
+const callApi = (url) => {
   fetch(url)
-    .then((res) => {
-      const json = res.json();
-      return json;
-    })
-    .then((json) => {
-      console.log(json); // {publishingOffice: "気象庁", ...
-    });
+    .then(async (res) => ({
+      status: res.status,
+      json: await res.json(),
+    }))
+    .then(console.log);
 };
 
-const test2 = () => {
-  fetch(url)
-    .then((res) => {
-      const json = res.json();
-      return { json };
-    })
-    .then(({ json }) => {
-      console.log(json); // Promise {<pending>}
-    });
-};
-
-const test3 = () => {
-  fetch(url)
-    .then((res) => {
-      const json = res.json();
-      return [json];
-    })
-    .then((arr) => {
-      const json = arr[0];
-      console.log(json); // Promise {<pending>}
-    });
-};
-
-test1();
-test2();
-test3();
+callApi('https://www.jma.go.jp/bosai/forecast/data/overview_week/130000.json');
+/**
+{status: 200, json: {…}}
+json:
+headTitle: "関東甲信地方週間天気予報"
+publishingOffice: "気象庁"
+reportDatetime: "2021-03-08T10:35:00+09:00"
+text: "予報期間　３月９日から３月１５日まで↵　向こう一週間は、期間のはじめと終わりに晴れる日もありますが、期間の中頃は気圧の谷や湿った空気の影響で雲が広がりやすく、雨の降る日があるでしょう。↵　最高気温は、平年より高い日が多く、期間のはじめと終わりは平年よりかなり高い日もある見込みです。最低気温は、平年より高い日が多く、期間の後半は平年よりかなり高い日もあるでしょう。↵　降水量は、平年並か平年より多い見込みです。"
+__proto__: Object
+status: 200
+__proto__: Object
+ */
